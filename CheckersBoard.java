24206b4613ce80cb774c8b694f464b97c13290f6
PeterMartinez <peter.m.martinez92@gmail.com>
2017-06-20 14:06:10 -0400

    Initial setup of CheckersProgram.java and CheckersBoard.java, no actual code yet

---
+++ CheckersBoard.java
@@ -0,0 +1,16 @@
+/*
+	
+	Author: Peter Martinez
+	Date Created: 6/20/2017
+	Last Updated: 6/20/2017
+
+	This class keeps track of the location of each piece on a checkers
+	a checkers board as well as managing the movement of said pieces.
+
+*/
+
+
+public class CheckersBoard {
+
+
+}
---
+++ CheckersProgram.java
@@ -0,0 +1,21 @@
+/*
+	Author: Peter Martinez
+	Created: 6/20/2017
+	Last Updated: 6/20/2017
+	
+	This is the main driver for the checkers game program. This file 
+	will create an instance of a CheckersBoard, which takes care of all 
+	of the logic for the game. This class will simply loop and use the
+	CheckersBoard class to pass along information to the UI for display.
+*/
+import java.util.scanner;
+
+
+public class CheckersProgram {
+
+	public static void main(String[] args) {
+
+
+
+	}
+}
